3a8a6969553734b39520aa707c55a4a18aeda1d1
AkashiCoin <55268546+AkashiCoin@users.noreply.github.com>
2023-01-11 16:44:05 +0800

    feat(action): support auto build

--- vite.config.ts
+++ vite.config.ts
@@ -3,22 +3,27 @@ import vue from '@vitejs/plugin-vue'
 import styleImport from 'vite-plugin-style-import'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    styleImport({
-      libs: [{
-        libraryName: 'element-plus',
-        esModule: true,
-        ensureStyleFile: true,
-        resolveStyle: (name) => {
-          name = name.slice(3)
-          return `element-plus/packages/theme-chalk/src/${name}.scss`;
-        },
-        resolveComponent: (name) => {
-          return `element-plus/lib/${name}`;
-        },
-      }]
-    })
-  ]
+export default defineConfig(({ command, mode }) => {
+  return {
+    plugins: [
+      vue(),
+      styleImport({
+        libs: [{
+          libraryName: 'element-plus',
+          esModule: true,
+          ensureStyleFile: true,
+          resolveStyle: (name) => {
+            name = name.slice(3)
+            return `element-plus/packages/theme-chalk/src/${name}.scss`;
+          },
+          resolveComponent: (name) => {
+            return `element-plus/lib/${name}`;
+          },
+        }]
+      })
+    ],
+    base: command === 'build' && mode === 'cdn'
+      ? 'https://cdn1.tianli0.top/gh/AkashiCoin/imgbed@web-dist/'
+      : '/'
+  }
 })
