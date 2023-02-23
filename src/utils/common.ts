d0e616584d7c0155a0103e991d3f66734aecb93b
AkashiCoin <55268546+AkashiCoin@users.noreply.github.com>
2023-02-23 18:08:53 +0800

    fix: paht of server-worker.js

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -1,57 +1,57 @@
 
 // 注册 service worker
 async function register() {
-    const registed = await navigator.serviceWorker.getRegistration("./");
-    if (registed?.active) return registed.active;
-  
-    const swRegistration = await navigator.serviceWorker.register("service-worker.js", {
-      scope: "./",
-    });
-  
-    const sw = swRegistration.installing! || swRegistration.waiting!;
-  
-    let listen: any;
-  
-    return new Promise<ServiceWorker>((resolve) => {
-      sw.addEventListener(
-        "statechange",
-        (listen = () => {
-          if (sw.state === "activated") {
-            sw.removeEventListener("statechange", listen);
-            resolve(swRegistration.active!);
-          }
-        })
-      );
-    });
-  }
-  
-  // 向 service worker 申请下载资源
-  export async function createDownloadStream(filename: string, filesize: number) {
-    const { port1, port2 } = new MessageChannel();
-  
-    const sw = await register();
-  
-    sw.postMessage({ filename, filesize }, [port2]);
-  
-    return new Promise<WritableStream>((r) => {
-      port1.onmessage = (e) => {
-        const iframe = document.createElement("iframe");
-        iframe.hidden = true;
-        iframe.src = e.data.download;
-        iframe.name = "iframe";
-        document.body.appendChild(iframe);
-        r(e.data.writable);
-      };
-    });
-  }
-  
+  const registed = await navigator.serviceWorker.getRegistration("./");
+  if (registed?.active) return registed.active;
+
+  const swRegistration = await navigator.serviceWorker.register("/service-worker.js", {
+    scope: "./",
+  });
+
+  const sw = swRegistration.installing! || swRegistration.waiting!;
+
+  let listen: any;
+
+  return new Promise<ServiceWorker>((resolve) => {
+    sw.addEventListener(
+      "statechange",
+      (listen = () => {
+        if (sw.state === "activated") {
+          sw.removeEventListener("statechange", listen);
+          resolve(swRegistration.active!);
+        }
+      })
+    );
+  });
+}
+
+// 向 service worker 申请下载资源
+export async function createDownloadStream(filename: string, filesize: number) {
+  const { port1, port2 } = new MessageChannel();
+
+  const sw = await register();
+
+  sw.postMessage({ filename, filesize }, [port2]);
+
+  return new Promise<WritableStream>((r) => {
+    port1.onmessage = (e) => {
+      const iframe = document.createElement("iframe");
+      iframe.hidden = true;
+      iframe.src = e.data.download;
+      iframe.name = "iframe";
+      document.body.appendChild(iframe);
+      r(e.data.writable);
+    };
+  });
+}
+
 export const randomString = (len: number) => {
   len = len || 8;
   let $chars = "ABCDEFGHJKMNPQRSTWXYZabcdefhijkmnprstwxyz2345678";
   let maxPos = $chars.length;
   let result = "";
   for (let i = 0; i < len; i++) {
-      result += $chars.charAt(Math.floor(Math.random() * maxPos));
+    result += $chars.charAt(Math.floor(Math.random() * maxPos));
   }
   return result;
 }
